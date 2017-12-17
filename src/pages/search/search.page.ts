71c4b4638b24ac1be79a7a67c0491073f88992f3
Munsif Musthafa <immsf5@gmail.com>
2017-12-18 00:58:26 +0530

    Work on Geolocation

--- src/pages/search/search.page.ts
+++ src/pages/search/search.page.ts
@@ -1,5 +1,5 @@
 import { Component, NgZone, ElementRef, ViewChild } from "@angular/core";
-import { NavController } from "ionic-angular";
+import { NavController, Platform } from "ionic-angular";
 import { MapsAPILoader } from "@agm/core";
 import { FormControl } from "@angular/forms";
 import {} from "@types/googlemaps";
@@ -14,91 +14,58 @@ declare var google: any;
     templateUrl: "search.page.html"
 })
 export class SearchPage {
-
-    public latitude: number;
-    public longitude: number;
     public searchControl: FormControl;
-    public zoom: number;
+    public searchbar: String = "0";
 
-    @ViewChild('search',  { read: ElementRef }) public searchElementRef: ElementRef;
+    @ViewChild("search", { read: ElementRef })
+    public searchElementRef: ElementRef;
 
     constructor(
         public navCtrl: NavController,
         private ngZone: NgZone,
-        private mapsAPILoader: MapsAPILoader
-    ) {}
+        private mapsAPILoader: MapsAPILoader,
+        private platform: Platform
+    ) { }
 
     ngOnInit() {
-        // console.log(this.searchElementRef.nativeElement)
-        // console.log(this.searchElementRef.nativeElement.getElementsByTagName('input'));
+        this.setCurrentPosition();
         this.searchControl = new FormControl();
-        this.mapsAPILoader.load().then(
-            () => {
-                let typedLocation = this.searchElementRef.nativeElement.getElementsByTagName('input').search;
-                console.log(typedLocation )
-                let autocomplete = new google.maps.places.Autocomplete(typedLocation , { types: ["address"] });
-                autocomplete.addListener('place_changed', () => {
-                    this.ngZone.run(() => {
-                        let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-                        console.log("place", place);
-                        if(place.geometry === undefined || place.geometry === null) {
-                            return;
-                        }
-                    });
-                })
-            }
-        );
+        this.mapsAPILoader.load().then(() => {
+            let typedLocation = this.searchElementRef.nativeElement.getElementsByTagName("input").search;
+            let autocomplete = new google.maps.places.Autocomplete(typedLocation, { types: ["address"] });
+            autocomplete.addListener("place_changed", () => {
+                this.ngZone.run(() => {
+                    let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+                    console.log("place", place);
+                    if ( place.geometry === undefined || place.geometry === null ) {
+                        return;
+                    } else {
+                        console.log("Error Fetching Suggestion");
+                    }
+                });
+            });
+        });
     }
 
-    // ngOnInit() {
-        // console.log("this.searchElementRef", this.searchElementRef)
-        //set google maps defaults
-        // this.zoom = 4;
-        // this.latitude = 39.8282;
-        // this.longitude = -98.5795;
-
-        //create search FormControl
-        // this.searchControl = new FormControl();
-
-        // //set current position
-        // this.setCurrentPosition();
-
-        // //load Places Autocomplete
-        // this.mapsAPILoader.load().then(() => {
-        //     let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, { types: ["address"] });
-
-        //     autocomplete.addListener("place_changed", () => {
-        //         this.ngZone.run(() => {
-        //             //get the place result
-        //             let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-        //             console.log('place', place)                    
-                    
-        //             //verify result
-        //             if ( place.geometry === undefined || place.geometry === null ) {
-        //                 console.log('lol...........................................')
-        //                 return;
-        //             }
-        //             //set latitude, longitude and zoom
-        //             // this.latitude = place.geometry.location.lat();
-        //             // this.longitude = place.geometry.location.lng();
-        //             // this.zoom = 12;
-        //         });
-        //     });
-        // });
-    // }
-
-    private setCurrentPosition() {
+    setCurrentPosition() {
         if ("geolocation" in navigator) {
-          navigator.geolocation.getCurrentPosition((position) => {
-            this.latitude = position.coords.latitude;
-            this.longitude = position.coords.longitude;
-            this.zoom = 12;
-
-            // var geocoder = new google.maps.Geocoder;
-            // geocoder.geocode({'location': pos}, function(results, status) {}
-          });
+            navigator.geolocation.getCurrentPosition(position => {
+                let pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                var geocoder = new google.maps.Geocoder();
+                geocoder.geocode({ location: pos }, function(results, status) {
+                    if (status == "OK") {
+                        this.searchbar = results[0].formatted_address;
+                        console.log("results", results[0].formatted_address);
+                    } else {
+                        console.log("Error Fetching Location");
+                    }
+                });
+            });
         }
-      }
+    }
 
     showResultsPage(fuelType, radius) {
         this.navCtrl.push(ResultsPage, {
@@ -106,9 +73,4 @@ export class SearchPage {
             radius: radius
         });
     }
-
-    searchLocation() {
-        console.log("inside searchLocation()")        
-    }
-
 }
